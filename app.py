4e05e71fd98f2af2b7795b7014ad02b7ed1940ab
hongjoyce <hongjoycejiang@gmail.com>
2023-01-12 12:16:53 -0600

    change app run port

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def predict():
 #If we import this file (module) to another file then __name__ == app (which is the name of this python file).
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host = '0.0.0.0', port = 5000)
